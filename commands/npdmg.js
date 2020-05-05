9d8c3460a7ebf538f3084fe06f7d8e22c242c024
maketakunai <12004080+maketakunai@users.noreply.github.com>
2020-05-04 17:44:16 -0700

    added possible toggle of np values for servs without np interludes

--- commands/npdmg.js
+++ commands/npdmg.js
@@ -1,6 +1,6 @@
 const emoji = require("../data/emoji.json")
 const servantList = require("../data/servant_db.json");
-const lastUpdated = "04/16/2020"
+const lastUpdated = "05/04/2020"
 const master = require("../data/master.json")
 
 exports.run = (client, message, args) => {
